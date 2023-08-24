4abae42082aa921672f039a667199308050bfebd
DJ Daniels <dj.daniels@upstart.com>
2023-08-24 16:33:40 -0600

    next theme provider setup

--- src/eight-queens/src/pages/_app.tsx
+++ src/eight-queens/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { ThemeProvider } from '../components/ThemeProvider'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
